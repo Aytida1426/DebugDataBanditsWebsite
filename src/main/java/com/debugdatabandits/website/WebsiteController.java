ef6d3a77b000f412a87895f24d49ae53626a0c0a
AdityaS1426 <adisurapaneni@gmail.com>
2021-02-28 10:18:27 -0800

    Updated WebsiteController.java.

--- src/main/java/com/debugdatabandits/website/WebsiteController.java
+++ src/main/java/com/debugdatabandits/website/WebsiteController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController {
+public class WebsiteController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String index() {
