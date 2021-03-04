f583c8163c1137f68e642b4f4cc5ae32ee7fd210
AdityaS1426 <adisurapaneni@gmail.com>
2021-03-03 18:12:10 -0800

    Made changes to Greeting.

--- src/main/java/com/debugdatabandits/website/GreetingController.java
+++ src/main/java/com/debugdatabandits/website/GreetingController.java
@@ -1,14 +1,14 @@
 package com.debugdatabandits.website;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return "Greeting";
     }
 }
