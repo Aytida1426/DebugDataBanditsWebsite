8432bbd50c72beda459bb73e588bae8af9a2cd81
AdityaS1426 <adisurapaneni@gmail.com>
2021-03-03 18:04:51 -0800

    Added Greeting.

--- src/main/java/com/debugdatabandits/website/GreetingController.java
+++ src/main/java/com/debugdatabandits/website/GreetingController.java
@@ -1,16 +1,21 @@
 package com.debugdatabandits.website;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@SpringBootApplication
+@RestController
 public class GreetingController {
 
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingController.class, args);
+    }
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "Greeting";
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }
